5cf21ae1d9fab2e4f7b77ba752669a8a1a699b8b
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-10-12 14:12:50 +0530

    profile picture changes

--- src/Details.js
+++ src/Details.js
@@ -3,7 +3,7 @@
 import logogradient from "./assets/logo.svg";
 import logo from "./assets/logo2.svg";
 // Profile Image
-import profile from "./assets/profile.jpg";
+import profile from "./assets/profile.jpeg";
 // Tech stack images
 import html from "./assets/techstack/html.png";
 import css from "./assets/techstack/css.png";
