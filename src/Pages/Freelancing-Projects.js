9495d6767323fd9834547e6d8167cb9344b8f931
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-08-07 21:24:28 +0530

    minor changes

--- src/Pages/Freelancing-Projects.js
+++ src/Pages/Freelancing-Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { flutterProjectDetails, freelancingProjects } from "../Details";
+import { freelancingProjects } from "../Details";
 import { Cards } from "../Components/Cards-Carousel/Cards";
 
 function FreelancingProjects() {
@@ -7,7 +7,7 @@ function FreelancingProjects() {
     <main id="project" className="container mx-auto max-width pt-0 mb-0">
       <section>
         <h1 className="text-xl text-dark-heading dark:text-light-heading md:text-2xl xl:text-3xl xl:leading-tight font-bold">
-          Freelacing Projects
+          Freelancing Projects
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-x-5 gap-y-5">
           <Cards data={freelancingProjects} />
